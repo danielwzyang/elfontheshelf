49171786911ef86942f141bcfa13273450491c60
Alex <ashao60@stuy.edu>
2026-05-11 14:37:25 -0400

    Some work on main.c

--- src/main.c
+++ src/main.c
@@ -1,3 +1,20 @@
-int main(void) {
-    return 0;
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+#include <elf.h>
+
+
+/* THE FIRST PART:
+	1. Read header
+
+
+*/
+int read_header(){
+	
+}
+
+
+int main() {
+	return 0;
+
 }
