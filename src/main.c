cee07eb3d9e16afa1ad7d00abe71889da23333e9
Alex <ashao60@stuy.edu>
2026-05-12 14:08:10 -0400

    Preparing to merge with main

--- src/main.c
+++ src/main.c
@@ -1,4 +1,3 @@
-
 #include "structs.h"
 
 /* THE FIRST PART:
@@ -6,21 +5,17 @@
 	https://linuxvox.com/blog/reading-the-contents-of-an-elf-section-programmatically/
 	1. Read header
 
-
-
 */
 
-
 int read_header(char *name){
-	int fp;
-	fp = open(name, O_RDONLY);
+	int fp = open(name, O_RDONLY);
 	
+
 	return 0;
 }
 
 
-
-int main(){
+int main(int argc, char **argv){
+	read_header(argv[2]);
 	return 0;
-
 }
--- src/structs.h
+++ src/structs.h
@@ -6,12 +6,14 @@
 	#include <elf.h>
 	#include <sys/mman.h>
 	#include <sys/types.h>
+	#include <unistd.h>
+	#include <fcntl.h>
 
 	struct elf_info{
 		void *start_elf;
 		void *info_bit_1;
 		void *this_is_not_final_owen_add_whatever_you_need;
-	}
+	};
 
 
 #endif
