69413bfc19764318fe50a6781ac09a6215b6bdec
Alex <ashao60@stuy.edu>
2026-05-12 14:50:28 -0400

    Work in class, file doesn't compile

--- src/main.c
+++ src/main.c
@@ -7,17 +7,23 @@
 
 */
 
-void nerror(char *s, char *desc){
-	printf(" \e[1;31m%s\e[0m\n", s);
-	printf("\e[1;31m\t|%s\e[0m\n", desc);
+// Coloured Error Printer
+void nerror(char *error, char *desc, char fatal){
+	printf(" \e[1;91m%s:\e[0m\n", error);
+	printf("\e[0;91m\t|%s\e[0m\n", desc);
+	if(fatal) exit(1);
 }
 
 int read_header(char *name){
 	int fp = open(name, O_RDONLY);
-	if (fp < 0){
-		nerror("Open elf:", "Failed to open elf file");
-	}
+	struct stat inf;
+	if (fp < 0) 
+		nerror("Open elf", "Failed to open elf file", 1);
+	if (fstat(fp, &inf)) 
+		nerror("ELF STAT", "Unable to retreive stats of ELF file", 1);
 
+	const char *mm;
+	mm = mmap(NULL, stat.st_size, PROT_READ, MAP_PRIVATE, fp, 0);
 	return 0;
 }
 
@@ -26,6 +32,7 @@ int main(int argc, char **argv){
 	read_header(argv[2]);
 	return 0;
 }
+
 
 
 
--- src/structs.h
+++ src/structs.h
@@ -3,9 +3,10 @@
 	#include <stdio.h>
 	#include <stdlib.h>
 	#include <sys/stat.h>
-	#include <elf.h>
 	#include <sys/mman.h>
 	#include <sys/types.h>
+	#include <string.h>
+	#include <elf.h>
 	#include <unistd.h>
 	#include <fcntl.h>
 
