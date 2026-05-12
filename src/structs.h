639ff2a14ec03a17ba767426bdbd74ad8e49f79d
Owen-Zeng <owenzeng1002@gmail.com>
2026-05-12 13:47:51 -0500

    function headers to h file

--- src/structs.h
+++ src/structs.h
@@ -33,4 +33,8 @@ struct InjectionMetadata {
   int text_segment_index;
 };
 
+void write_injection(struct Elf64_Header header, struct InjectionMetadata);
+int read_header(char *name);
+void nerror(char *s, char *desc);
+
 #endif
